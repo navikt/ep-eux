48ad8db1dca0e5af2f2122ae029e1cf97d65fda4
NN <nicolas.nordhagen@nav.no>
2021-02-16 13:07:11 +0100

    Støtte ukjent SedStatus + status "new"

--- src/main/kotlin/no/nav/eessi/pensjon/eux/model/document/ForenkletSED.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/eux/model/document/ForenkletSED.kt
@@ -1,5 +1,6 @@
 package no.nav.eessi.pensjon.eux.model.document
 
+import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonValue
 import no.nav.eessi.pensjon.eux.model.sed.SedType
@@ -12,19 +13,22 @@ data class ForenkletSED(
 ) {
     fun harGyldigStatus(): Boolean = (status == SedStatus.SENT || status == SedStatus.RECEIVED)
 
-    fun erKansellert(): Boolean = (status == SedStatus.CANCELLED)
+    fun erKansellert(): Boolean = status == SedStatus.CANCELLED
 
     override fun toString(): String = "ForenkletSED(id=$id, type=$type, status=$status)"
 }
 
 @Suppress("unused")
 enum class SedStatus(@JsonValue private val value: String) {
+    NEW("new"),
+    SENT("sent"),
     EMPTY("empty"),
     RECEIVED("received"),
-    SENT("sent"),
     CANCELLED("cancelled");
 
     companion object {
+        @JvmStatic
+        @JsonCreator
         fun fra(value: String?): SedStatus? {
             return if (value == null) null
             else values().firstOrNull { it.value == value }
