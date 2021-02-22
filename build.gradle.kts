7bcc51d2b50113b8662b54ec566d4e4e46c38389
NN <nicolas.nordhagen@nav.no>
2021-02-22 14:29:31 +0100

    bump metrics & sts

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,8 +37,8 @@ dependencies {
     implementation(kotlin("stdlib", "1.4.30"))
 
     implementation("io.micrometer:micrometer-registry-prometheus:1.4.2")
-    implementation("no.nav.eessi.pensjon:ep-security-sts:0.0.8")
-    implementation("no.nav.eessi.pensjon:ep-metrics:0.4.0")
+    implementation("no.nav.eessi.pensjon:ep-security-sts:0.0.14")
+    implementation("no.nav.eessi.pensjon:ep-metrics:0.4.2")
 
     // Spring
     implementation("org.springframework:spring-web:$springVersion")
