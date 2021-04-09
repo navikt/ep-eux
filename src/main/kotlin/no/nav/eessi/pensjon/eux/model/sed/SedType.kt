e7cf0cb61cd3c47a24caf8ec3718280f8bb17458
moamini <mo.amini@nav.no>
2021-04-09 14:12:26 +0200

    legger til SedType companion metoder

--- src/main/kotlin/no/nav/eessi/pensjon/eux/model/sed/SedType.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/eux/model/sed/SedType.kt
@@ -30,6 +30,7 @@ enum class SedType(val beskrivelse: String) {
     P3000_ES("Landsspesifikk informasjon - Spania"),
     P3000_FI("Landsspesifikk informasjon - Finland"),
     P3000_FR("Landsspesifikk informasjon - Frankrike"),
+    P3000_GR("Landsspesifikk informasjon - Hellas"),
     P3000_HR("Landsspesifikk informasjon - Kroatia"),
     P3000_HU("Landsspesifikk informasjon - Ungarn"),
     P3000_IE("Landsspesifikk informasjon - Irland"),
@@ -104,10 +105,36 @@ enum class SedType(val beskrivelse: String) {
     companion object {
         @JvmStatic
         @JsonCreator
-        fun fra(value: String?): SedType? {
-            return value?.let {
-                values().firstOrNull { type ->  type.name == it }
+        fun from(s: String): SedType? {
+            return try {
+                valueOf(s)
+            } catch (e: Exception) {
+                null
             }
         }
+
+        @JvmStatic
+        fun isValidSEDType(input: String): Boolean {
+            return try {
+                valueOf(input)
+                true
+            } catch (ia: IllegalArgumentException) {
+                false
+            }
+        }
+    }
+
+    /**
+     * Sjekker om SedTypen er gyldig for prefill.
+     * Basert på listen over SedTyper som var tilgjengelig i appen fra commit 6fc9b6c7298262ff9b4a53f70549215a700bc40a
+     */
+    fun kanPrefilles(): Boolean {
+        return this in listOf(
+            SedType.P2000, P2100, P2200, P4000, P6000, P5000, P7000, P8000, P9000, P10000, P14000, P15000, X005,
+            H020, H021, H070, H120, H121, P12000, P13000, P1000, P1100, P11000, P3000_FR, P3000_RO, P3000_IE,
+            P3000_HU, P3000_LT, P3000_IS, P3000_UK, P3000_NO, P3000_IT, P3000_SI, P3000_MT, P3000_BE, P3000_EE,
+            P3000_AT, P3000_BG, P3000_LI, P3000_DK, P3000_SE, P3000_FI, P3000_PL, P3000_DE, P3000_ES, P3000_PT,
+            P3000_LV, P3000_SK, P3000_NL, P3000_GR, P3000_HR, P3000_CY, P3000_LU, P3000_CH, P3000_CZ
+        )
     }
 }
