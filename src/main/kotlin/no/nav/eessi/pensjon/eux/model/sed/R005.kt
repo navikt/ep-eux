ade653f7c1fa7c988c4446532d10990ab6010452
moamini <mo.amini@nav.no>
2021-04-23 10:48:21 +0200

    Legger til R005 sedklasse

---
+++ src/main/kotlin/no/nav/eessi/pensjon/eux/model/sed/R005.kt
@@ -0,0 +1,24 @@
+package no.nav.eessi.pensjon.eux.model.sed
+
+import com.fasterxml.jackson.annotation.JsonProperty
+
+class R005(
+    @JsonProperty("sed")
+    override var type: SedType = SedType.P2200,
+    override val sedGVer: String? = "4",
+    override var sedVer: String? = "1",
+    override var nav: Nav? = null,
+    override var pensjon: Pensjon?,
+    val tilbakekreving: Tilbakekreving? = null
+
+) : SED(type, sedGVer, sedVer, nav, pensjon)
+
+data class Tilbakekreving(
+    val feilutbetaling: Feilutbetaling? = null,
+    val status: Status? = null
+)
+
+data class Feilutbetaling(val ytelse: Ytelse?)
+
+data class Ytelse(val type: String?)
+data class Status(val type: String?)
