e65df99ab2e14606670efadae1db68f623867b37
moamini <mo.amini@nav.no>
2021-04-27 10:15:26 +0200

    Legger til tilbakekreving for R005

--- src/main/kotlin/no/nav/eessi/pensjon/eux/model/sed/R005.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/eux/model/sed/R005.kt
@@ -9,5 +9,6 @@ class R005(
     override var sedVer: String? = "1",
     override var nav: Nav? = null,
     override var pensjon: Pensjon?,
+    val tilbakekreving: Tilbakekreving? = null
 
 ) : SED(type, sedGVer, sedVer, nav, pensjon)
