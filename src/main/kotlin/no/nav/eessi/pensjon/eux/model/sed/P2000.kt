d8ec4418180c81dd09c6e2bebd393188f7aafedf
moamini <mo.amini@nav.no>
2021-06-16 12:31:17 +0200

    microcommit legger til P2000 klasse

---
+++ src/main/kotlin/no/nav/eessi/pensjon/eux/model/sed/P2000.kt
@@ -0,0 +1,20 @@
+package no.nav.eessi.pensjon.eux.model.sed
+
+import com.fasterxml.jackson.annotation.JsonProperty
+
+class P2000(
+    @JsonProperty("sed")
+    override var type: SedType = SedType.P2000,
+    override val sedGVer: String? = "4",
+    override var sedVer: String? = "1",
+    override var nav: Nav? = null,
+    override var pensjon: Pensjon?
+) : SED(type, sedGVer, sedVer, nav, pensjon) {
+
+    fun validerForKravinit() {
+        if(nav?.bruker?.person?.sivilstand != null && nav?.bruker?.person?.statsborgerskap != null) return true
+        else false
+    }
+}
+
+
