9b3ffb99de76ed11537557dfe3b971b2daccf1d6
mariampervez <mariam.pervez@nav.no>
2021-08-06 15:32:07 +0200

    Fjerner bruker fra P6000Pensjon

--- src/main/kotlin/no/nav/eessi/pensjon/eux/model/sed/P6000Pensjon.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/eux/model/sed/P6000Pensjon.kt
@@ -1,7 +1,6 @@
 package no.nav.eessi.pensjon.eux.model.sed
 
 class P6000Pensjon(
-    val bruker: Bruker? = null,
     val gjenlevende: Bruker? = null,
     val reduksjon: List<ReduksjonItem>? = null,
     val vedtak: List<VedtakItem>? = null,
