532f1061fbb53370ba6c6e74d8fab2e46c74342c
mariampervez <mariam.pervez@nav.no>
2023-01-06 10:48:38 +0100

    F - Legger inn SedHendelse med tilhørende serdes test
    -Legger også inn ep-personoppslag avhengighet
    
    Co-authored-by: Daniel.skarpas@nav.no

---
+++ src/main/kotlin/no/nav/eessi/pensjon/eux/model/SedHendelse.kt
@@ -0,0 +1,22 @@
+package no.nav.eessi.pensjon.eux.model
+
+import no.nav.eessi.pensjon.eux.model.buc.BucType
+import no.nav.eessi.pensjon.personoppslag.Fodselsnummer
+
+data class SedHendelse(
+    val id: Long? = 0,
+    val sedId: String? = null,
+    val sektorKode: String,
+    val bucType: BucType?,
+    val rinaSakId: String,
+    val avsenderId: String? = null,
+    val avsenderNavn: String? = null,
+    val avsenderLand: String? = null,
+    val mottakerId: String? = null,
+    val mottakerNavn: String? = null,
+    val mottakerLand: String? = null,
+    val rinaDokumentId: String,
+    val rinaDokumentVersjon: String,
+    val sedType: SedType? = null,
+    val navBruker: Fodselsnummer? = null
+)
---
+++ src/test/kotlin/no/nav/eessi/pensjon/eux/model/SedHendelseTest.kt
@@ -0,0 +1,51 @@
+package no.nav.eessi.pensjon.eux.model
+
+import no.nav.eessi.pensjon.eux.model.buc.BucType
+import no.nav.eessi.pensjon.personoppslag.Fodselsnummer
+import no.nav.eessi.pensjon.utils.mapJsonToAny
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class SedHendelseTest {
+
+    @Test
+    fun `Serdes test for SedHendelse`() {
+        val sedhendelseJson = """
+            {
+              "id" : 0,
+              "sedId" : null,
+              "sektorKode" : "P",
+              "bucType" : "P_BUC_01",
+              "rinaSakId" : "123456479867",
+              "avsenderId" : null,
+              "avsenderNavn" : "avsenderNavn",
+              "avsenderLand" : "avsenderLand",
+              "mottakerId" : null,
+              "mottakerNavn" : "mottakerNavn",
+              "mottakerLand" : "mottakerLand",
+              "rinaDokumentId" : "SOME_DOKUMENT_ID",
+              "rinaDokumentVersjon" : "SOME_RINADOKUMENT_VERSION",
+              "sedType" : "P2200",
+              "navBruker" : "22190656256"
+            }
+        """.trimIndent()
+
+        val sedHendelse = mapJsonToAny<SedHendelse>(sedhendelseJson)
+
+        assertEquals(sedHendelse.id, 0)
+        assertEquals(sedHendelse.sedId, null)
+        assertEquals(sedHendelse.sektorKode, "P")
+        assertEquals(sedHendelse.bucType, BucType.P_BUC_01)
+        assertEquals(sedHendelse.rinaSakId, "123456479867")
+        assertEquals(sedHendelse.avsenderId, null)
+        assertEquals(sedHendelse.avsenderNavn, "avsenderNavn")
+        assertEquals(sedHendelse.avsenderLand, "avsenderLand")
+        assertEquals(sedHendelse.mottakerId, null)
+        assertEquals(sedHendelse.mottakerNavn, "mottakerNavn")
+        assertEquals(sedHendelse.mottakerLand, "mottakerLand")
+        assertEquals(sedHendelse.rinaDokumentId, "SOME_DOKUMENT_ID")
+        assertEquals(sedHendelse.sedType, SedType.P2200)
+        assertEquals(sedHendelse.navBruker, Fodselsnummer.fra("22190656256"))
+    }
+
+}
