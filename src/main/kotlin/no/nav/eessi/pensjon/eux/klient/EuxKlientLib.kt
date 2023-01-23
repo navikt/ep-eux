6d724186891f95f3ad13118002b1af7e73b032bb
Daniel Skarpås <dskarpas@gmail.com>
2023-01-23 13:57:12 +0100

    F - Flytter hentAlleDokumentfiler fra journalføring til ep-eux

--- src/main/kotlin/no/nav/eessi/pensjon/eux/klient/EuxKlientLib.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/eux/klient/EuxKlientLib.kt
@@ -1,12 +1,14 @@
 package no.nav.eessi.pensjon.eux.klient
 
 import no.nav.eessi.pensjon.eux.model.buc.Buc
+import no.nav.eessi.pensjon.eux.model.document.SedDokumentfiler
 import no.nav.eessi.pensjon.eux.model.sed.SED
 import no.nav.eessi.pensjon.utils.mapJsonToAny
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
+import org.springframework.web.client.HttpStatusCodeException
 import org.springframework.web.client.RestTemplate
 
 class EuxKlientLib(private val euxRestTemplate: RestTemplate) {
@@ -47,5 +49,26 @@ class EuxKlientLib(private val euxRestTemplate: RestTemplate) {
         )
 
         return response.statusCode == HttpStatus.OK || response.statusCode == HttpStatus.NO_CONTENT
+    }
+
+    fun hentAlleDokumentfiler(rinaSakId: String, dokumentId: String): SedDokumentfiler? {
+        logger.info("Henter PDF for SED og tilhørende vedlegg for rinaSakId: $rinaSakId , dokumentId: $dokumentId")
+        return execute {
+            euxRestTemplate.getForObject(
+                "/buc/$rinaSakId/sed/$dokumentId/filer",
+                SedDokumentfiler::class.java
+            )
+        }
+    }
+
+    private fun <T> execute(block: () -> T): T? {
+        try {
+            return block.invoke()
+        } catch (ex: Exception) {
+            if (ex is HttpStatusCodeException && ex.statusCode == HttpStatus.NOT_FOUND)
+                return null
+            logger.error("Ukjent feil oppsto: ", ex)
+            throw ex
+        }
     }
 }
