b399c0fbbc38d42b7007ce32c6b8465a1da06ba6
mariampervez <mariam.pervez@nav.no>
2023-04-25 14:53:21 +0200

    R - Legger til gjenlevende på P8000Pensjon
    Co-authored-by: Daniel.skarpas@nav.no

--- src/main/kotlin/no/nav/eessi/pensjon/eux/model/sed/P8000Pensjon.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/eux/model/sed/P8000Pensjon.kt
@@ -3,6 +3,7 @@ package no.nav.eessi.pensjon.eux.model.sed
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 
 class P8000Pensjon(
+    val gjenlevende: Bruker? = null,
     val anmodning: AnmodningOmTilleggsInfo? = null,
     val ytterligeinformasjon: String? = null,
     val vedlegg: List<String> ? = null
