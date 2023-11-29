df07a75cc68c6cd24f861729696ddca30ccdccf0
Daniel Skarpås <dskarpas@gmail.com>
2023-11-29 22:50:57 +0100

    F - Endrer key for caching for hentsed

--- src/test/kotlin/no/nav/eessi/pensjon/eux/klient/EuxKlientCacheTest.kt
+++ src/test/kotlin/no/nav/eessi/pensjon/eux/klient/EuxKlientCacheTest.kt
@@ -4,18 +4,19 @@ import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
 import no.nav.eessi.pensjon.eux.config.EuxCacheConfig
+import no.nav.eessi.pensjon.eux.config.SED_CACHE
+import no.nav.eessi.pensjon.utils.toJson
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.cache.CacheManager
 import org.springframework.context.annotation.Bean
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig
 import org.springframework.web.client.RestTemplate
 
-
 @SpringJUnitConfig(classes = [EuxCacheConfig::class, EuxKlientCacheTest.Config::class])
 class EuxKlientCacheTest {
-    private val RINASAK_ID = 123
 
     @Autowired
     lateinit var euxKlientLib: EuxKlientLib
@@ -23,35 +24,33 @@ class EuxKlientCacheTest {
     @Autowired
     lateinit var euxRestTemplate: RestTemplate
 
+    @Autowired
+    lateinit var cacheManager: CacheManager
+
+    private val RINASAK_ID = "123"
+    private val sedIds = listOf("11111", "11111", "11111", "22222")
+
     @BeforeEach
     fun setup() {
-        every {
-            euxRestTemplate.getForObject("/buc/${RINASAK_ID}/sed/11111", String::class.java )
-        } returns javaClass.getResource("/sed/P2000-NAV.json")!!.readText()
-        every {
-            euxRestTemplate.getForObject("/buc/${RINASAK_ID}/sed/22222", String::class.java )
-        } returns javaClass.getResource("/sed/P2000-NAV.json")!!.readText()
-        every {
-            euxRestTemplate.getForObject("/buc/${RINASAK_ID}/sed/33333", String::class.java )
-        } returns javaClass.getResource("/sed/P2000-NAV.json")!!.readText()
+        sedIds.forEach {
+            every {
+                euxRestTemplate.getForObject("/buc/${RINASAK_ID}/sed/$it", String::class.java)
+            } returns javaClass.getResource("/sed/P2000-NAV.json")!!.readText()
+        }
     }
 
     @Test
     fun `euxKlient skal cache henting av SED og kun kalle ekstern API én gang`() {
-        euxKlientLib.hentSedJson("${RINASAK_ID}", "11111")
-        euxKlientLib.hentSedJson("${RINASAK_ID}", "11111")
-        euxKlientLib.hentSedJson("${RINASAK_ID}", "22222")
-        euxKlientLib.hentSedJson("${RINASAK_ID}", "33333")
 
-        verify(exactly = 1) {
-            euxRestTemplate.getForObject("/buc/${RINASAK_ID}/sed/11111", String::class.java )
+        sedIds.forEach {
+            euxKlientLib.hentSedJson(RINASAK_ID, it)
         }
-        verify(exactly = 1) {
-            euxRestTemplate.getForObject("/buc/${RINASAK_ID}/sed/22222", String::class.java)
+        sedIds.distinct().forEach {
+            verify(exactly = 1) {
+                euxRestTemplate.getForObject("/buc/${RINASAK_ID}/sed/$it", String::class.java)
+            }
         }
-        verify(exactly = 1) {
-            euxRestTemplate.getForObject("/buc/${RINASAK_ID}/sed/33333", String::class.java)
-        }
+        println(cacheManager.getCache(SED_CACHE)?.nativeCache?.toJson())
     }
 
     @TestConfiguration
