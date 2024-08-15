62bf96c0d7d6632f353edb83306da9d88775676f
Daniel Skarpås <dskarpas@gmail.com>
2024-08-15 14:56:53 +0200

    F - Utvider P12000 for vurdering av uføre

--- src/main/kotlin/no/nav/eessi/pensjon/eux/model/sed/P12000.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/eux/model/sed/P12000.kt
@@ -2,10 +2,21 @@ package no.nav.eessi.pensjon.eux.model.sed
 
 import com.fasterxml.jackson.annotation.JsonProperty
 import no.nav.eessi.pensjon.eux.model.SedType
+import no.nav.eessi.pensjon.utils.mapJsonToAny
+import no.nav.eessi.pensjon.utils.toJson
 
 class P12000(
     @JsonProperty("sed")
     override val type: SedType = SedType.P12000,
     override val nav: Nav? = null,
     override val pensjon: P12000Pensjon?
-) : SED(type, nav = nav)
+) : SED(type, nav = nav){
+
+    fun harUforePensjonType(): Boolean {
+        return mapJsonToAny<P12000>(this.toJson())
+            .pensjon?.pensjoninfo
+            ?.firstOrNull()
+            ?.betalingsdetaljer
+            ?.pensjonstype == "02"
+    }
+}
