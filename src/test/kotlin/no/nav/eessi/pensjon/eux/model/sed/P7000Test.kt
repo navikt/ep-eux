a2f403ef7e4ace94b2018109b8a800aa4e3044b0
mariampervez <mariam.pervez@nav.no>
2024-11-15 13:43:38 +0100

    t - Endrer tester ifht YtelserItem med nye verdier

--- src/test/kotlin/no/nav/eessi/pensjon/eux/model/sed/P7000Test.kt
+++ src/test/kotlin/no/nav/eessi/pensjon/eux/model/sed/P7000Test.kt
@@ -12,6 +12,7 @@ class P7000Test {
     fun mapJsonToP7000() {
 
         val p7000 = mapJsonToAny<P7000>(validNavjson())
+        println("@@@@@@@@ $p7000")
 
         Assertions.assertEquals(2, p7000.pensjon?.samletVedtak?.tildeltepensjoner?.size)
         Assertions.assertEquals(1, p7000.pensjon?.samletVedtak?.avslag?.size)
@@ -21,7 +22,7 @@ class P7000Test {
         val dkAvslag = p7000.pensjon?.samletVedtak?.avslag?.firstOrNull { it.pin?.land == "DK" }
 
         Assertions.assertEquals("2021-07-20", noTildelt?.revurderingtidsfrist)
-        Assertions.assertEquals("123123123123", noTildelt?.ytelser?.first()?.beloep?.first()?.beloepBrutto)
+        Assertions.assertEquals("123123123123", noTildelt?.ytelser?.first()?.beloepsListe?.first()?.beloepBrutto)
         Assertions.assertEquals(
             "4.1.8.2 revjrflkasdfjasdf asdfasd fadfg",
             noTildelt?.adressatForRevurdering?.first()?.adressatforrevurdering
@@ -29,7 +30,7 @@ class P7000Test {
         Assertions.assertEquals("01", noTildelt?.pensjonType)
 
         Assertions.assertEquals("2021-07-20", seTildelt?.revurderingtidsfrist)
-        Assertions.assertEquals("41", seTildelt?.ytelser?.first()?.beloep?.first()?.beloepBrutto)
+        Assertions.assertEquals("41", seTildelt?.ytelser?.first()?.beloepsListe?.first()?.beloepBrutto)
         Assertions.assertEquals("4.1.8.2.1", seTildelt?.adressatForRevurdering?.first()?.adressatforrevurdering)
         Assertions.assertEquals("01", seTildelt?.pensjonType)
 
@@ -39,6 +40,7 @@ class P7000Test {
         Assertions.assertEquals("01", dkAvslag?.pensjonType)
 
         val p7000json = p7000.toJsonSkipEmpty()
+        println("@@@@@@@@ $p7000json")
         JSONAssert.assertEquals(p7000json, validNavjson(), false)
 
     }
@@ -46,117 +48,117 @@ class P7000Test {
     private fun validNavjson() : String {
 
         return """
-            {
-              "pensjon" : {
-                "samletVedtak" : {
-                  "tildeltepensjoner" : [ {
-                    "adressatForRevurdering" : [ {
-                      "adressatforrevurdering" : "4.1.8.2 revjrflkasdfjasdf asdfasd fadfg"
-                    } ],
-                    "revurderingtidsfrist" : "2021-07-20",
-                    "pensjonType" : "01",
-                    "ytelser" : [ {
-                      "beloep" : [ {
-                        "valuta" : "NOK",
-                        "betalingshyppighetytelse" : "03",
-                        "beloepBrutto" : "123123123123"
-                      } ],
-                      "annenbetalingshyppighetytelse" : "4.1.6.6.1 annen",
-                      "startdatoutbetaling" : "2021-07-20",
-                      "sluttdatoretttilytelse" : "2021-07-20"
-                    } ],
-                    "startdatoPensjonsRettighet" : "2020-05-01",
-                    "reduksjonsGrunn" : "02",
-                    "institusjon" : {
-                      "saksnummer" : "23123",
-                      "sektor" : "alle",
-                      "personNr" : "29065325959",
-                      "land" : "NO",
-                      "institusjonsid" : "NO:NAVAT01",
-                      "institusjonsnavn" : "NAV ACCEPTANCE TEST 01"
-                    }
-                  }, {
-                    "institusjon" : {
-                      "institusjonsid" : "SE:ACC2001",
-                      "sektor" : "alle",
-                      "institusjonsnavn" : "The Swedish Pensions Agency",
-                      "personNr" : "21317212",
-                      "saksnummer" : "32142342134234234234234",
-                      "land" : "SE"
-                    },
-                    "ytelser" : [ {
-                      "annenbetalingshyppighetytelse" : "4.1.6.6.1",
-                      "startdatoutbetaling" : "2021-07-20",
-                      "beloep" : [ {
-                        "beloepBrutto" : "41",
-                        "betalingshyppighetytelse" : "03",
-                        "valuta" : "SEK"
-                      } ],
-                      "sluttdatoretttilytelse" : "2021-07-20"
-                    } ],
-                    "revurderingtidsfrist" : "2021-07-20",
-                    "pensjonType" : "01",
-                    "adressatForRevurdering" : [ {
-                      "adressatforrevurdering" : "4.1.8.2.1"
-                    } ],
-                    "startdatoPensjonsRettighet" : "2021-07-20",
-                    "reduksjonsGrunn" : "01"
+        {
+          "pensjon" : {
+            "samletVedtak" : {
+              "tildeltepensjoner" : [ {
+                "adressatForRevurdering" : [ {
+                  "adressatforrevurdering" : "4.1.8.2 revjrflkasdfjasdf asdfasd fadfg"
+                } ],
+                "revurderingtidsfrist" : "2021-07-20",
+                "pensjonType" : "01",
+                "ytelser" : [ {
+                  "beloepsListe" : [ {
+                    "valuta" : "NOK",
+                    "beloepBrutto" : "123123123123",
+                    "betalingshyppighetytelse" : "maaned_12_per_aar"
                   } ],
-                  "avslag" : [ {
-                    "pin" : {
-                      "identifikator" : "34234'1234",
-                      "land" : "DK",
-                      "institusjon" : {
-                        "institusjonsnavn" : "ACC_The Danish Debt Collection Agency",
-                        "institusjonsid" : "DK:10429",
-                        "saksnummer" : "2342341234513452456"
-                      },
-                      "sektor" : "alle"
-                    },
-                    "dato" : "2021-07-20",
-                    "begrunnelse" : "06",
-                    "tidsfristForRevurdering" : "5.1.5.1 tid",
-                    "pensjonType" : "01"
-                  } ]
-                },
-                "gjenlevende" : {
-                  "person" : {
-                    "fornavn" : "LUGUBER",
-                    "kjoenn" : "M",
-                    "pin" : [ {
-                      "land" : "NO",
-                      "identifikator" : "14115224590"
-                    } ],
-                    "foedselsdato" : "1952-11-14",
-                    "etternavn" : "VEGGPRYD"
-                  }
-                }
-              },
-              "sedVer" : "1",
-              "nav" : {
-                "bruker" : {
-                  "person" : {
-                    "fornavn" : "æøå",
-                    "kjoenn" : "K",
-                    "etternavn" : "æøå",
-                    "etternavnvedfoedsel" : "Avdød",
-                    "foedselsdato" : "1952-12-07",
-                    "pin" : [ {
-                      "identifikator" : "07125224002",
-                      "land" : "NO"
-                    } ]
-                  }
-                },
-                "eessisak" : [ {
-                  "institusjonsnavn" : "NAV ACCEPTANCE TEST 07",
-                  "saksnummer" : "22919987",
+                  "annenbetalingshyppighetytelse" : "4.1.6.6.1 annen",
+                  "startdatoutbetaling" : "2021-07-20",
+                  "sluttdatoretttilytelse" : "2021-07-20"
+                } ],
+                "startdatoPensjonsRettighet" : "2020-05-01",
+                "reduksjonsGrunn" : "02",
+                "institusjon" : {
+                  "saksnummer" : "23123",
+                  "sektor" : "alle",
+                  "personNr" : "29065325959",
                   "land" : "NO",
-                  "institusjonsid" : "NO:NAVAT07"
-                } ]
-              },
-              "sedGVer" : "4",
-              "sed" : "P7000"
+                  "institusjonsid" : "NO:NAVAT01",
+                  "institusjonsnavn" : "NAV ACCEPTANCE TEST 01"
+                }
+              }, {
+                "institusjon" : {
+                  "institusjonsid" : "SE:ACC2001",
+                  "sektor" : "alle",
+                  "institusjonsnavn" : "The Swedish Pensions Agency",
+                  "personNr" : "21317212",
+                  "saksnummer" : "32142342134234234234234",
+                  "land" : "SE"
+                },
+                "ytelser" : [ {
+                  "annenbetalingshyppighetytelse" : "4.1.6.6.1",
+                  "startdatoutbetaling" : "2021-07-20",
+                  "beloepsListe" : [ {
+                    "beloepBrutto" : "41",
+                    "betalingshyppighetytelse" : "maaned_12_per_aar",
+                    "valuta" : "SEK"
+                  } ],
+                  "sluttdatoretttilytelse" : "2021-07-20"
+                } ],
+                "revurderingtidsfrist" : "2021-07-20",
+                "pensjonType" : "01",
+                "adressatForRevurdering" : [ {
+                  "adressatforrevurdering" : "4.1.8.2.1"
+                } ],
+                "startdatoPensjonsRettighet" : "2021-07-20",
+                "reduksjonsGrunn" : "01"
+              } ],
+              "avslag" : [ {
+                "pin" : {
+                  "identifikator" : "34234'1234",
+                  "land" : "DK",
+                  "institusjon" : {
+                    "institusjonsnavn" : "ACC_The Danish Debt Collection Agency",
+                    "institusjonsid" : "DK:10429",
+                    "saksnummer" : "2342341234513452456"
+                  },
+                  "sektor" : "alle"
+                },
+                "dato" : "2021-07-20",
+                "begrunnelse" : "06",
+                "tidsfristForRevurdering" : "5.1.5.1 tid",
+                "pensjonType" : "01"
+              } ]
+            },
+            "gjenlevende" : {
+              "person" : {
+                "fornavn" : "LUGUBER",
+                "kjoenn" : "M",
+                "pin" : [ {
+                  "land" : "NO",
+                  "identifikator" : "14115224590"
+                } ],
+                "foedselsdato" : "1952-11-14",
+                "etternavn" : "VEGGPRYD"
+              }
             }
+          },
+          "sedVer" : "1",
+          "nav" : {
+            "bruker" : {
+              "person" : {
+                "fornavn" : "æøå",
+                "kjoenn" : "K",
+                "etternavn" : "æøå",
+                "etternavnvedfoedsel" : "Avdød",
+                "foedselsdato" : "1952-12-07",
+                "pin" : [ {
+                  "identifikator" : "07125224002",
+                  "land" : "NO"
+                } ]
+              }
+            },
+            "eessisak" : [ {
+              "institusjonsnavn" : "NAV ACCEPTANCE TEST 07",
+              "saksnummer" : "22919987",
+              "land" : "NO",
+              "institusjonsid" : "NO:NAVAT07"
+            } ]
+          },
+          "sedGVer" : "4",
+          "sed" : "P7000"
+        }
         """.trimIndent()
     }
 
