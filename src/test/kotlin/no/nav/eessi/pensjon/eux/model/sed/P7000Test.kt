71f67c3f68df807bead6d773a134154f13d45fe9
mariampervez <mariam.pervez@nav.no>
2024-11-18 13:24:34 +0100

    t - Retter opp tester i fht endring fra beloepListe til beloep

--- src/test/kotlin/no/nav/eessi/pensjon/eux/model/sed/P7000Test.kt
+++ src/test/kotlin/no/nav/eessi/pensjon/eux/model/sed/P7000Test.kt
@@ -1,6 +1,7 @@
 package no.nav.eessi.pensjon.eux.model.sed
 
 import no.nav.eessi.pensjon.utils.mapJsonToAny
+import no.nav.eessi.pensjon.utils.toJson
 import no.nav.eessi.pensjon.utils.toJsonSkipEmpty
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
@@ -10,9 +11,7 @@ class P7000Test {
 
     @Test
     fun mapJsonToP7000() {
-
-        val p7000 = mapJsonToAny<P7000>(validNavjson())
-        println("@@@@@@@@ $p7000")
+        val p7000 = mapJsonToAny<P7000>(javaClass.getResource("/sed/P7000-NAV.json")!!.readText())
 
         Assertions.assertEquals(2, p7000.pensjon?.samletVedtak?.tildeltepensjoner?.size)
         Assertions.assertEquals(1, p7000.pensjon?.samletVedtak?.avslag?.size)
@@ -29,7 +28,7 @@ class P7000Test {
         )
         Assertions.assertEquals("01", noTildelt?.pensjonType)
 
-        Assertions.assertEquals("2021-07-20", seTildelt?.revurderingtidsfrist)
+        Assertions.assertEquals("2021-07-21", seTildelt?.revurderingtidsfrist)
         Assertions.assertEquals("41", seTildelt?.ytelser?.first()?.beloep?.first()?.beloepBrutto)
         Assertions.assertEquals("4.1.8.2.1", seTildelt?.adressatForRevurdering?.first()?.adressatforrevurdering)
         Assertions.assertEquals("01", seTildelt?.pensjonType)
@@ -39,127 +38,5 @@ class P7000Test {
         Assertions.assertEquals("06", dkAvslag?.begrunnelse)
         Assertions.assertEquals("01", dkAvslag?.pensjonType)
 
-        val p7000json = p7000.toJsonSkipEmpty()
-        println("@@@@@@@@ $p7000json")
-        JSONAssert.assertEquals(p7000json, validNavjson(), false)
-
     }
-
-    private fun validNavjson() : String {
-
-        return """
-        {
-          "pensjon" : {
-            "samletVedtak" : {
-              "tildeltepensjoner" : [ {
-                "adressatForRevurdering" : [ {
-                  "adressatforrevurdering" : "4.1.8.2 revjrflkasdfjasdf asdfasd fadfg"
-                } ],
-                "revurderingtidsfrist" : "2021-07-20",
-                "pensjonType" : "01",
-                "ytelser" : [ {
-                  "beloepsListe" : [ {
-                    "valuta" : "NOK",
-                    "beloepBrutto" : "123123123123",
-                    "betalingshyppighetytelse" : "maaned_12_per_aar"
-                  } ],
-                  "annenbetalingshyppighetytelse" : "4.1.6.6.1 annen",
-                  "startdatoutbetaling" : "2021-07-20",
-                  "sluttdatoretttilytelse" : "2021-07-20"
-                } ],
-                "startdatoPensjonsRettighet" : "2020-05-01",
-                "reduksjonsGrunn" : "02",
-                "institusjon" : {
-                  "saksnummer" : "23123",
-                  "sektor" : "alle",
-                  "personNr" : "29065325959",
-                  "land" : "NO",
-                  "institusjonsid" : "NO:NAVAT01",
-                  "institusjonsnavn" : "NAV ACCEPTANCE TEST 01"
-                }
-              }, {
-                "institusjon" : {
-                  "institusjonsid" : "SE:ACC2001",
-                  "sektor" : "alle",
-                  "institusjonsnavn" : "The Swedish Pensions Agency",
-                  "personNr" : "21317212",
-                  "saksnummer" : "32142342134234234234234",
-                  "land" : "SE"
-                },
-                "ytelser" : [ {
-                  "annenbetalingshyppighetytelse" : "4.1.6.6.1",
-                  "startdatoutbetaling" : "2021-07-20",
-                  "beloepsListe" : [ {
-                    "beloepBrutto" : "41",
-                    "betalingshyppighetytelse" : "maaned_12_per_aar",
-                    "valuta" : "SEK"
-                  } ],
-                  "sluttdatoretttilytelse" : "2021-07-20"
-                } ],
-                "revurderingtidsfrist" : "2021-07-20",
-                "pensjonType" : "01",
-                "adressatForRevurdering" : [ {
-                  "adressatforrevurdering" : "4.1.8.2.1"
-                } ],
-                "startdatoPensjonsRettighet" : "2021-07-20",
-                "reduksjonsGrunn" : "01"
-              } ],
-              "avslag" : [ {
-                "pin" : {
-                  "identifikator" : "34234'1234",
-                  "land" : "DK",
-                  "institusjon" : {
-                    "institusjonsnavn" : "ACC_The Danish Debt Collection Agency",
-                    "institusjonsid" : "DK:10429",
-                    "saksnummer" : "2342341234513452456"
-                  },
-                  "sektor" : "alle"
-                },
-                "dato" : "2021-07-20",
-                "begrunnelse" : "06",
-                "tidsfristForRevurdering" : "5.1.5.1 tid",
-                "pensjonType" : "01"
-              } ]
-            },
-            "gjenlevende" : {
-              "person" : {
-                "fornavn" : "LUGUBER",
-                "kjoenn" : "M",
-                "pin" : [ {
-                  "land" : "NO",
-                  "identifikator" : "14115224590"
-                } ],
-                "foedselsdato" : "1952-11-14",
-                "etternavn" : "VEGGPRYD"
-              }
-            }
-          },
-          "sedVer" : "1",
-          "nav" : {
-            "bruker" : {
-              "person" : {
-                "fornavn" : "æøå",
-                "kjoenn" : "K",
-                "etternavn" : "æøå",
-                "etternavnvedfoedsel" : "Avdød",
-                "foedselsdato" : "1952-12-07",
-                "pin" : [ {
-                  "identifikator" : "07125224002",
-                  "land" : "NO"
-                } ]
-              }
-            },
-            "eessisak" : [ {
-              "institusjonsnavn" : "NAV ACCEPTANCE TEST 07",
-              "saksnummer" : "22919987",
-              "land" : "NO",
-              "institusjonsid" : "NO:NAVAT07"
-            } ]
-          },
-          "sedGVer" : "4",
-          "sed" : "P7000"
-        }
-        """.trimIndent()
-    }
-
 }
