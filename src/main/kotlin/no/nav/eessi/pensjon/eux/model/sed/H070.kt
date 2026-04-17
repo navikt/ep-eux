0fc6fcee81e0f586b36776d8d88ef47f799e5dd2
mariampervez <mariam.pervez@nav.no>
2026-04-17 15:34:23 +0200

    f - Legger til H070

---
+++ src/main/kotlin/no/nav/eessi/pensjon/eux/model/sed/H070.kt
@@ -0,0 +1,10 @@
+package no.nav.eessi.pensjon.eux.model.sed
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import no.nav.eessi.pensjon.eux.model.SedType
+
+class H070(
+    @JsonProperty("sed")
+    override var type: SedType,
+    override var nav: Nav? = null,
+) : SED(type, nav = nav)
